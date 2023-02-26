4bfa7c0a8644c8447396f48562647a3343c9e6c0
Daniel Artine Mastub <daniel.mastub@stoneage.com.br>
2023-02-25 22:35:33 -0300

    Aula-5

--- UsuariosApi/obj/Debug/net6.0/UsuariosApi.AssemblyInfo.cs
+++ UsuariosApi/obj/Debug/net6.0/UsuariosApi.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("4374b8ab-3c14-4292-93e3-a5f8be4a4749")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsuariosApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
